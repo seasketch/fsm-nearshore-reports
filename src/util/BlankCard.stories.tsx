373eb7dc9a8024bb8b88002329d7fe59b485361e
Abby Vath Meyer <avmey@ucsb.edu>
2024-03-11 16:45:10 +0000

    Update tradeoff with Crow data

--- src/util/BlankCard.stories.tsx
+++
@@ -1,4 +0,0 @@
-import { BlankCard } from "./BlankCard";
-import { registerExampleStories } from "@seasketch/geoprocessing/storybook";
-
-registerExampleStories("Project/Components/BlankCard", BlankCard);
--- src/util/OverlapCard.stories.tsx
+++
@@ -1,4 +0,0 @@
-import { OverlapCard } from "./OverlapCard";
-import { registerExampleStories } from "@seasketch/geoprocessing/storybook";
-
-registerExampleStories("Project/Components/OverlapCard", OverlapCard);
