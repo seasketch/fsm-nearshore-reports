c6055215c164840b13db4f6785942b288554fb6b
Abby Vath Meyer <avmey@ucsb.edu>
2024-03-27 21:37:08 +0000

    OUS demographics

--- src/components/OusDemographic.stories.tsx
+++
@@ -1,4 +0,0 @@
-import { registerExampleStories } from "@seasketch/geoprocessing/storybook";
-import { OusDemographics } from "./OusDemographic";
-
-registerExampleStories("Project/Components/OusDemographic", OusDemographics);
--- src/components/ViabilityPage.tsx
+++ src/components/ViabilityPage.tsx
@@ -4,7 +4,6 @@ import { SketchAttributesCard } from "@seasketch/geoprocessing/client-ui";
 import { OUSCard } from "./OUSCard";
 import { GroupCard } from "./Group";
 import { ReportProps } from "../util/ReportProp";
-import { OusDemographics } from "./OusDemographic";
 
 const ReportPage: React.FunctionComponent<ReportProps> = (props) => {
   return (
@@ -14,10 +13,6 @@ const ReportPage: React.FunctionComponent<ReportProps> = (props) => {
       {props.geographyId === "kosrae" ? (
         <>
           <OUSCard geographyId={props.geographyId} printing={props.printing} />
-          <OusDemographics
-            geographyId={props.geographyId}
-            printing={props.printing}
-          />
         </>
       ) : (
         <></>
