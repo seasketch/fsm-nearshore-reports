7959802199a437c4baf0e35d1e33ccfb21b4e9a5
Abby Vath Meyer <avmey@ucsb.edu>
2026-02-23 23:33:36 +0000

    Update settings

--- src/util/Settings.tsx
+++ src/util/Settings.tsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Gear as BaseGear, CloudArrowUpFill } from "@styled-icons/bootstrap";
 import { styled } from "styled-components";
 import {
   Dropdown,
   SimpleButtonStyled,
 } from "@seasketch/geoprocessing/client-ui";
-import project from "../../project/projectClient.js";
-import { Datasource } from "@seasketch/geoprocessing/client-core";
+import datasources from "../../project/datasources.json" with { type: "json" };
 
 const DropdownItemStyled = styled(SimpleButtonStyled)`
   font-size: 12px;
@@ -21,69 +20,34 @@ const Gear = styled(BaseGear)`
   }
 `;
 
+const escapeCsv = (s: string) =>
+  /[,"\n]/.test(s) ? `"${s.replace(/"/g, '""')}"` : s;
+
+const csv =
+  "datasourceId,lastUpdated\n" +
+  datasources
+    .map(
+      (ds: { datasourceId: string; lastUpdated?: string }) =>
+        `${escapeCsv(ds.datasourceId)},${escapeCsv(ds.lastUpdated ?? "NA")}`,
+    )
+    .join("\n");
+
+const blob = new Blob([csv], { type: "text/csv" });
+const url = URL.createObjectURL(blob);
+
 export const Settings: React.FunctionComponent = () => {
-  const [lastModified, setLastModified] = useState<Record<string, string>>({});
-
-  useEffect(() => {
-    (async () => setLastModified(await fetchDatasourcesLastModified()))();
-  }, []);
-
-  const blob = new Blob([JSON.stringify({ lastModified })], {
-    type: "application/json",
-  });
-  const url = URL.createObjectURL(blob);
-
   return (
     <Dropdown
       titleElement={<Gear size={18} title="Settings" />}
       placement="top-end"
     >
-      <a download="DataUpdateHistory.json" href={url}>
+      <a download="DataUpdateHistory.csv" href={url}>
         <DropdownItemStyled>
           <span style={{ verticalAlign: "middle" }}>
-            <CloudArrowUpFill size={16} /> View Data Update History
+            <CloudArrowUpFill size={16} /> Data Update History
           </span>
         </DropdownItemStyled>
       </a>
     </Dropdown>
   );
 };
-
-/**
- * Fetches the last modified date of all internal datasources in the project
- * @returns Record object with {datasourceId: last modified date} for each datasource
- */
-async function fetchDatasourcesLastModified() {
-  const datasources: Datasource[] = project.datasources;
-  const modDates: Record<string, string> = {};
-
-  await Promise.all(
-    datasources.map(async (ds: Datasource) => {
-      try {
-        const url = project.getDatasourceUrl(ds);
-        const lastModified = await fetchLastModifiedHeader(url);
-        modDates[ds.datasourceId] = lastModified ? lastModified : "NA";
-      } catch (error) {
-        console.log(`Error fetching ${ds.datasourceId} modification date`);
-      }
-    }),
-  );
-
-  return modDates;
-}
-
-/**
- * Fetches the Last-Modified date of an internal datasource in s3
- * @param url URL of the datasource in s3
- * @returns last modified date of that datasource
- */
-async function fetchLastModifiedHeader(url: string): Promise<string | null> {
-  try {
-    const response = await fetch(url, { method: "HEAD" });
-    return response.ok && response.headers.has("Last-Modified")
-      ? response.headers.get("Last-Modified")
-      : null;
-  } catch {
-    return null;
-  }
-}
